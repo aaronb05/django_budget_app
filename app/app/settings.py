dc1cea6721505196547e8ef2ceb52c47561ec921
aaronb05 <aboyles05@gmail.com>
2022-05-16 10:12:52 -0400

    hide secret

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ge@6)rgsgc13x=)%-_)_^=(qip0@m)j9#g-e_-qo%e!ncmpa^-'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
